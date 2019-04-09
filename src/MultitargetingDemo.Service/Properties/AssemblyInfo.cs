072c3a7cb90a64ae8f8332557a03c8a3452601b8
Jay Brummels <jay.brummels@csgi.com>
2019-04-09 14:15:59 -0500

    Converting all possible projects to SDK Style Projects aka https://github.com/hvanbakel/CsprojToVs2017

--- src/MultitargetingDemo.Service/Properties/AssemblyInfo.cs
+++ src/MultitargetingDemo.Service/Properties/AssemblyInfo.cs
@@ -10,7 +10,17 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MultitargetingDemo.Service")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright(@"Copyright (c) 2019 Jay Brummels
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the ""Software""), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
